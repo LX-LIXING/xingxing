9e749643dc00188b1734531216d1e7af9b0e5b0e
李星 <lixing@togeek.cn>
2018-09-12 13:53:40 +0800

    spring boot add filter and filter order

---
+++ springbootbase/src/main/java/cn/springboot/configFile/ConfigFile.java
@@ -0,0 +1,60 @@
+package cn.springboot.configFile;
+
+import cn.springboot.filter.MyWebFilter;
+import cn.springboot.filter.MyWebFilter2;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+/*
+ * 自定义过滤器并排序
+ *
+ * 1.自定义过滤器
+ * 2.自定义此文件
+ * 3.在启动类上加上@ServletComponentScan
+ * */
+
+@Configuration
+public class ConfigFile {
+   @Bean
+   public FilterRegistrationBean filterRegistrationBean() {
+      FilterRegistrationBean registration = new FilterRegistrationBean();
+
+      registration.setFilter(MyWebFilter1());
+      //过滤器名称
+      registration.setName("MyWebFilter1");
+      //拦截路径
+      registration.addUrlPatterns("/*");
+      //设置顺序
+      registration.setOrder(10);
+
+      return registration;
+   }
+
+   @Bean
+   public FilterRegistrationBean filterRegistrationBean2() {
+      FilterRegistrationBean registration = new FilterRegistrationBean();
+
+      registration.setFilter(MyWebFilter2());
+
+      registration.setName("MyWebFilter2");
+      //拦截路径
+      registration.addUrlPatterns("/*");
+      //设置顺序
+      registration.setOrder(5);
+
+      return registration;
+   }
+
+   @Bean
+   public Filter MyWebFilter1() {
+      return new MyWebFilter();
+   }
+
+   @Bean
+   public Filter MyWebFilter2() {
+      return new MyWebFilter2();
+   }
+}
---
+++ springbootbase/src/main/java/cn/springboot/filter/MyWebFilter2.java
@@ -0,0 +1,41 @@
+package cn.springboot.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/*
+* 1.定义类并实现Filter接口
+* 2.在启动类上加上@ServletComponentScan
+* */
+
+@WebFilter
+@Slf4j
+public class MyWebFilter2 implements Filter {
+
+   @Override
+   public void init(FilterConfig filterConfig) throws ServletException {
+      log.info("--------------------filter:初始化2");
+   }
+
+   @Override
+   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+      ServletException
+   {
+      log.info("---------------------dofilter 请求处理2");
+
+      chain.doFilter(request, response);
+   }
+
+   @Override
+   public void destroy() {
+      log.info("---------------------filter:销毁2");
+   }
+}
